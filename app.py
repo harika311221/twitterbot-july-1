611b1de64f9bf9fd73e9a2cf9e07522d9783191b
harika311221 <92515877+harika311221@users.noreply.github.com>
2021-10-14 22:05:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,12 @@
 import time
 import tweepy
 import os
-
 consumer_key = os.getenv('consumer_key')
 consumer_secret=os.getenv('consumer_secret')
 access_token=os.getenv('access_token')
 access_token_secret=os.getenv('access_token_secret')
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
 api = tweepy.API(auth)
 print("Authentication approved")
 while True:
